a448213c9419c3b675e88bbb9a90ed87edb66210
JUNAA MEDIA <junaamedia@MacBook-Pro-de-JUNAA.local>
2025-12-01 19:13:53 +0100

    add top section

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ScrollView, Platform } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Platform,
+  Image,
+} from "react-native";
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import theMatrix from "./assets/images/the-matrix-cover.jpg";
 
 import Constants from "expo-constants";
 
@@ -21,8 +29,24 @@ export default function App() {
           <FontAwesome6 name="imdb" size={40} color="white" />
         </View>
         <View>
-          <View>
-            <Text style={{ color: "white" }}>The Matrix</Text>
+          <View style={styles.topSection}>
+            <Text style={{ color: "white", fontSize: 32 }}>The Matrix</Text>
+            <View style={{ flexDirection: "row", gap: 30 }}>
+              <Text style={{ color: "grey" }}>1999</Text>
+              <Text style={{ color: "grey" }}>PG13</Text>
+              <Text style={{ color: "grey" }}>2h16min</Text>
+              <Text style={{ color: "grey" }}>Action, Sci-Fi</Text>
+            </View>
+            <View style={styles.preview}>
+              <Image source={theMatrix} style={{ width: 140, height: 200 }} />
+              <View>
+                <Text style={{ color: "white" }}>
+                  Un pirate informatique découvre par de mystérieux rebelles la
+                  véritable nature de sa réalité et son rôle dans la guerre
+                  contre ceux qui la contrôlent.
+                </Text>
+              </View>
+            </View>
           </View>
           <View></View>
         </View>
@@ -42,4 +66,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     paddingLeft: 30,
   },
+  topSection: { padding: 10, backgroundColor: `#212121`, gap: 10 },
+  preview: { width: 230, paddingTop: 20, flexDirection: "row", gap: 10 },
 });
